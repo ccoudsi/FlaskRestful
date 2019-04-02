c628d7ba1bf026b96671447309b110e84e1429b6
ccoudsi <ccoudsi@coudsi.com>
2019-04-02 11:06:52 -0700

    fixed not importing db when running the app on Heroku.

--- app.py
+++ app.py
@@ -14,12 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # if object changed and no
 app.secret_key = 'jose'  # Secret phrase
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # will create new endpoint "/auth"
 
 api.add_resource(Store, '/store/<string:name>')
